60e67265a9b92c0b146d4d7c65540cc0b06363d2
Vadym Kalmykov <kvvad@ukr.net>
2019-12-24 23:27:41 +0200

    Initial commit

---
+++ src/Lesson1/Main.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	System.out.print("Enter name -> ");
+//        Scanner scanner = new Scanner(System.in);
+//        String name = scanner.nextLine();
+//        System.out.println("Hello " + name);
+        System.out.println("* * * *   * * * *        *          *       *");
+        System.out.println("*         *     *      *   *        * *   * *");
+        System.out.println("* *       * * * *     * * * *       *   *   *");
+        System.out.println("*         *          *       *      *       *");
+        System.out.println("* * * *   *         *         *     *       *");
+
+    }
+}
